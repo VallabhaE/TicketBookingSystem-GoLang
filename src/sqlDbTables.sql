f54d0b1053ddfb513177ada9b3c70b56ca4a28f3
Eswar Vallabha <ramagownieswar@karunya.edu.in>
2025-03-02 19:12:11 +0530

    Updayed

--- src/sqlDbTables.sql
+++ src/sqlDbTables.sql
@@ -1,4 +1,6 @@
 -- All SQL Table Quarrys will be Listed Here
 
 
-git config --global user.email
+git config --global user.email "eswar.vallabha@iouring.com"
+
+git config --global user.email "ramagownieswar@karunya.edu.in"
