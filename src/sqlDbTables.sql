7a323a010545cf79f4c4e6c84e2c8a45cf0c1e8b
VallabhaE <81346948+VallabhaE@users.noreply.github.com>
2025-10-19 19:24:50 +0530

    updated .sql file , database

--- src/sqlDbTables.sql
+++ src/sqlDbTables.sql
@@ -1,11 +1,9 @@
 -- All SQL Table Quarrys will be Listed Here
 
-
--- git config --global user.email "eswar.vallabha@iouring.com"
-
 -- git config --global user.email "ramagownieswar@karunya.edu.in"
 
 
+create databse MovieTicketBooking;
 
 use MovieTicketBooking;
 
